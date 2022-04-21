0478a3e469fc0290810f766238405ce8670bca9c
ylsama <ylongkaka3@gmail.com>
2022-04-22 01:57:38 +0700

    model base {} with braket

---
+++ 02_Day_Introduction_to_React/model.js
@@ -0,0 +1,7 @@
+const welcome = "Welcome to 30 Days Of React";
+const title = "Getting Started React";
+const subtitle = "JavaScript Library";
+const authorFirstName = "Asabeneh";
+const authorLastName = "Yetayeh";
+const date = "Oct 1, 2020";
+const techs = ["HTML", "CSS", "JavaScript"];
