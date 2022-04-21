938ce708cf0c31dd954eaf3ca4d2fabe21716388
ylsama <ylongkaka3@gmail.com>
2022-04-22 02:04:31 +0700

    Finished touch

--- 02_Day_Introduction_to_React/model.js
+++ 02_Day_Introduction_to_React/model.js
@@ -1,7 +1,10 @@
 const welcome = "Welcome to 30 Days Of React";
 const title = "Getting Started React";
 const subtitle = "JavaScript Library";
-const authorFirstName = "Asabeneh";
-const authorLastName = "Yetayeh";
+const author = {
+  firstName: "Asabeneh",
+  lastName: "Yetayeh",
+};
 const date = "Oct 1, 2020";
 const techs = ["HTML", "CSS", "JavaScript"];
+const copyright = "Copyright 2020";
